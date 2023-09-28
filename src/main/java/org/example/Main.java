2556a4712cf74e56ea86e8ac3fe71e518d16e3b5
Your Name <you@example.com>
2023-09-28 11:20:47 +0300

    added for loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,10 @@ public class Main { // test comment
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println(System.currentTimeMillis());
+        }
+
         System.out.println("Hello from another planet!");
     }
 }
