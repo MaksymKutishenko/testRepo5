8b76989b8a60ecb2d39e8851cdaec8c4205a895c
Your Name <you@example.com>
2023-09-28 11:45:17 +0300

    added delay

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,12 @@
 package org.example;
 
-public class Main { // test comment
-    public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
 
         for (int i = 0; i < 15; i++) {
             System.out.println(System.currentTimeMillis() / 1000);
+            Thread.sleep(1000);
         }
-
-        System.out.println("Hello from another planet!");
     }
 }
