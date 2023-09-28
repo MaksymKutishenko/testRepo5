7180d5bc4d30d6fd65c8def72ef49575e0f65cf2
MaksymKutishenko <143539884+MaksymKutishenko@users.noreply.github.com>
2023-09-28 11:41:12 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,8 @@ public class Main { // test comment
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(System.currentTimeMillis());
+        for (int i = 0; i < 15; i++) {
+            System.out.println(System.currentTimeMillis() / 1000);
         }
 
         System.out.println("Hello from another planet!");
